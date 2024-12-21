986654f53698c90694d59a16d4b16cf742dfe7cb
Pavun-KumarCH <pavun9848@gmail.com>
2024-12-21 13:59:19 +0530

    Added Initial Files

---
+++ live_text.py
@@ -0,0 +1,36 @@
+import os
+import asyncio
+import pyaudio
+from dotenv import load_dotenv
+from google import genai
+from google.genai.types import SpeechConfig, VoiceConfig, PrebuiltVoiceConfig
+
+load_dotenv()
+
+model_id = "gemini-2.0-flash-exp"
+client = genai.Client(api_key = os.getenv("GOOGLE_API_KEY"), http_options = {"api_version": "v1alpha"}) # Http option to define api version
+
+config = {"response_modalities": ["TEXT"]}  # we can also pass AUDIO
+
+
+async def chat_with_gemini():
+    # live client connection real time
+    async with client.aio.live.connect(model = model_id, config = config) as session: 
+        while True:
+
+            message = input("Pavan: ")
+            await session.send(message, end_of_turn = True) # end_of_turn we are teeling the model that we done speaking
+
+
+            # listening to Gemini's response
+            p = pyaudio.PyAudio()
+            stream = p.open(format = pyaudio.paInt16,
+                            channels = 1,
+                            rate = 24000,  # sample rate 24000 Hz
+                            output = True)
+            
+            async for response in session.receive():
+                print(f"Gemini 2.0: {response.text}")
+            
+if __name__ == "__main__":
+    asyncio.run(chat_with_gemini())
